6e5c7f6efcc909dae628e3eab1d22c6b2feb068c
ujjwal G <crystalninja0123@gmail.com>
2025-10-31 21:23:57 +0530

    final commit

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-const API_URL = "http://localhost:5000/api";
+const deployed = "https://task-management-5eed.onrender.com/api";
+const API_URL = deployed || "http://localhost:5000/api";
 
 interface RegisterProps {
   onRegister: (user: any, token: string) => void;
